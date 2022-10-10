cc35b3e070f1383b24525895d377e628e870076a
Bernie Rivas <bernierp96@gmail.com>
2022-10-09 22:36:06 -0400

    Simple Accordeon - Project Finished

---
+++ script.js
@@ -0,0 +1,19 @@
+let accordion = document.getElementsByClassName("accordion");
+
+for (let i = 0; i < accordion.length; i++) {
+    accordion[i].addEventListener("click", function() {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        var ico = this.firstElementChild;
+        if (panel.style.display === "flex") {
+            panel.style.display = "none"
+            ico.style.transform = "rotate(-90deg)";
+        } else {
+            panel.style.display = "flex"
+            ico.style.transform = "rotate(0deg)";
+        }
+      });
+}
